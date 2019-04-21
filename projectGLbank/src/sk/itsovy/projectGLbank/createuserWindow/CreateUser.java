94e22fae8af331d91b3a7f474ab1611d3d137eca
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-04-22 00:42:23 +0200

    added button to CreateUser and action event on button

--- projectGLbank/src/sk/itsovy/projectGLbank/createuserWindow/CreateUser.java
+++ projectGLbank/src/sk/itsovy/projectGLbank/createuserWindow/CreateUser.java
@@ -1,4 +1,8 @@
 package sk.itsovy.projectGLbank.createuserWindow;
 
+import javafx.event.ActionEvent;
+
 public class CreateUser {
+    public void createUserAction(ActionEvent actionEvent) {
+    }
 }
