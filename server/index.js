5aaf5c4a691ab2d7d022ef0e16d2d87881832590
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-05-27 10:54:29 +0200

    added cors in server,added request in login

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ let database = require('./database');
 const app = require('express')();
 const bodyParser = require("body-parser");
 
+var cors = require('cors');
+
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- web/index-script.js
+++ web/index-script.js
@@ -4,7 +4,21 @@ $(document).ready(() => {
 	let pass = $("#pass");
 	let submitBtn = $("#submitBtn");
 
+
 	submitBtn.click(() => {
 		console.log("test");
+
+		console.log(userName.val());
+		console.log(pass.val());
+
+		 $.ajax({
+		      type: "POST",
+		      contentType: "application/json; charset=utf-8",
+		      url: "http://localhost:3000/login",
+		      data: "{'login':'"+userName.val()+"','data2':'"+pass.val()+"'}",
+		      success: function (result) {
+		           console.log("it works");
+		      }
+		 });
 	});
 });
