08a05b36490e1cfc5cf2eaad465e7dc0b98b7eb3
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-05-27 13:14:23 +0200

    fixed login

--- web/index-script.js
+++ web/index-script.js
@@ -3,7 +3,8 @@ $(document).ready(() => {
 	let userName = $("#userName");
 	let pass = $("#pass");
 	let submitBtn = $("#submitBtn");
-
+	let form = $("#form");
+	let error = $("#errorWrap");
 
 	submitBtn.click(() => {
 		console.log("test");
@@ -16,9 +17,31 @@ $(document).ready(() => {
 		      contentType: "application/json; charset=utf-8",
 		      url: "http://localhost:3000/login",
 		      data: "{\"login\":\""+userName.val()+"\",\"password\":\""+pass.val()+"\"}",
-		      success: function (result) {
+		      success: function (result,textStatus,xhr) {
 		           console.log("it works");
-		      }
+		           console.log(textStatus);
+		           console.log(xhr.status);
+
+		           if(xhr.status == 200){
+		           	form.hide();
+		           }
+		           
+		      },
+		      error: function (xhr, textStatus, errorThrown) { 
+		      	console.log(xhr.status);
+		      	console.log(textStatus);
+		      	
+		      	if(xhr.status == 403){
+		      		error.empty();
+		      		error.append('<p id ="errorText">Error login account not found</p>');
+		      	}
+
+		      	if(xhr.status == 401){
+		      		error.empty();
+		      		error.append('<p id="errorText">Error bad login</p>');
+		      	}
+
+		      }	
 		 });
 	});
 });
