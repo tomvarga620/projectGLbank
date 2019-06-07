5a03a780bc4e54b208e7cfc54a1354367b977414
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-06-07 08:37:03 +0200

    functional logout

--- web/mainScript.js
+++ web/mainScript.js
@@ -1,11 +1,42 @@
 $(document).ready(() => {
 
 let logout = $(".logout");
-let logUser = sessionStorage.getItem("logUser");
+let logUser = JSON.parse(sessionStorage.getItem("logUser"));
 console.log(logUser);
+
+let login = logUser.login;
+let token = logUser.token;
+
+console.log(`${login} ${token}`);
 
 logout.click(() => {
 	console.log("logout test");
+		 $.ajax({
+		      type: "POST",
+		      contentType: "application/json; charset=utf-8",
+		      url: "http://localhost:3000/logout",
+		      data: "{\"login\":\""+login+"\",\"token\":\""+token+"\"}",
+		      success: function (result,textStatus,xhr) {
+		           console.log("it works");
+		           console.log(textStatus);
+		           console.log(xhr.status);
+
+		           if(xhr.status == 200){
+		           	sessionStorage.clear();
+		           	location.href = "index.html";
+		           }
+		           
+		      },
+		      error: function (xhr, textStatus, errorThrown) { 
+		      	console.log(xhr.status);
+		      	console.log(textStatus);
+
+		      	if(xhr.status == 401){
+		      		console.log(`Wrong credentials`);
+		      	}
+
+		      }	
+		 });	
 });
 
 });
